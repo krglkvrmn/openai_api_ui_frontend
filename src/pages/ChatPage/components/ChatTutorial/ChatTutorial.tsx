c5e2bb805648e97624ca49ae5f5bf071635a2138
krglkvrmn <kruglikov1911@mail.ru>
2024-04-23 03:25:16 +0400

    Update tutorial

--- src/pages/ChatPage/components/ChatTutorial/ChatTutorial.tsx
+++ src/pages/ChatPage/components/ChatTutorial/ChatTutorial.tsx
@@ -7,35 +7,38 @@ export function ChatTutorial() {
     return (
         <div className={styles.tutorialContainer}>
             <h2>How to use this app?</h2>
-            <ul className={styles.tutorialStepsList}>
-                <li className={styles.tutorialStepsListItem}>
-                    <span>Acquire an <a href="https://platform.openai.com/api-keys" className="href">OpenAI API key</a></span>
-                </li>
-                <li className={styles.tutorialStepsListItem}>
+            <div className={styles.tutorialStepsListContainer}>
+                <ul className={styles.tutorialStepsList}>
+                    <li className={styles.tutorialStepsListItem}>
+                        <span>Acquire an <a href="https://platform.openai.com/api-keys"
+                                            className="href">OpenAI API key</a></span>
+                    </li>
+                    <li className={styles.tutorialStepsListItem}>
                     <span>
-                        <MdKey />
+                        <MdKey/>
                         &nbsp;
                         <b className={styles.textPc}>Save the key</b>&nbsp;to your account on this site using the form on the top
                         &nbsp;
-                        <MdArrowUpward className={styles.arrowUp} />
+                        <MdArrowUpward className={styles.arrowUp}/>
                     </span>
-                    <br />
-                    Alternatively you can <b className={styles.textPc}>just paste the key</b> and not submit it.
-                    In such case your key will not be saved anywhere and the app will work as long as the key stays in the form
-                </li>
-                <li className={styles.tutorialStepsListItem}>
+                        <br/>
+                        Alternatively you can <b className={styles.textPc}>just paste the key</b> and not submit it.
+                        In such case your key will not be saved anywhere and the app will work as long as the key stays
+                        in the form
+                    </li>
+                    <li className={styles.tutorialStepsListItem}>
                     <span>
                         <b className={styles.textPc}>Choose an AI model</b> for an assistant. Currently available models are:
                         <b className={styles.textPc}> GPT-3.5-turbo</b> and <b className={styles.textPc}>GPT-4</b>
                     </span>
-                </li>
-                <li className={styles.tutorialStepsListItem}>
+                    </li>
+                    <li className={styles.tutorialStepsListItem}>
                     <span>
                         <b className={styles.textPc}>Enter your prompt</b>&nbsp; in the form below
                         <MdArrowDownward/> and submit it &nbsp;<MdSend className={styles.textPc}/>
                     </span>
-                </li>
-                <li className={styles.tutorialStepsListItem}>
+                    </li>
+                    <li className={styles.tutorialStepsListItem}>
                     <span>
                         You can <span
                         className={styles.textPc}>also configure a set of rules and instructions</span>&nbsp;
@@ -43,22 +46,24 @@ export function ChatTutorial() {
                         All such prompts are saved and you can re-use them from a right sidebar
                         <MdArrowForward/>
                     </span>
-                    <br/>
-                    <br/>
-                    <span>
+                        <br/>
+                        <br/>
+                        <span>
                         <MdOutlineModeEditOutline/>
-                        &nbsp;&mdash;&nbsp;
-                        The next prompt will be followed with assistant's reply
+                            &nbsp;&mdash;&nbsp;
+                            The next prompt will be followed with assistant's reply
                     </span>
-                    <br />
-                    <span>
+                        <br/>
+                        <span>
                         <GoGear/>
-                        &nbsp;&mdash;&nbsp;
-                        The next prompt (<span className={styles.textWc}>first message only</span>)
+                            &nbsp;&mdash;&nbsp;
+                            The next prompt (<span className={styles.textWc}>first message only</span>)
                         will be used as chat context
                     </span>
-                </li>
-            </ul>
+                    </li>
+                </ul>
+
+            </div>
         </div>
-);
+    );
 }
--- src/pages/ChatPage/components/EmptyChatPlaceholder/EmptyChatPlaceholder.tsx
+++ src/pages/ChatPage/components/EmptyChatPlaceholder/EmptyChatPlaceholder.tsx
@@ -1,11 +1,22 @@
 import styles from "./style.module.css";
 import {ChatTutorial} from "../ChatTutorial/ChatTutorial.tsx";
+import {useState} from "react";
 
 export function EmptyChatPlaceholder() {
+    const [showTutorial, setShowTutorial] = useState<boolean>(false);
     return (
         <div className={styles.emptyChatPlaceholder}>
-            <ChatTutorial/>
-            <h2>Have a nice chat!</h2>
+            {
+                showTutorial ?
+                    <ChatTutorial/> :
+                    <>
+                        <h2>Have a nice chat!</h2>
+                        <span className={styles.needHelpButton}
+                              onClick={() => setShowTutorial(prev => !prev)}>
+                            Need help?
+                        </span>
+                    </>
+            }
         </div>
     );
 }
